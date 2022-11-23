163b311efcd253488005b9fe77922f4002f69c55
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2022-11-23 12:05:12 +0100

    implement slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'andreasburghardt.de';
+export class AppComponent implements OnInit {
+  
+  images = ['laptop.jpg', 'sheets.jpg', 'music-pc.jpg'];
+  currentImage = 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    }, 8000);
+  }
+
 }
