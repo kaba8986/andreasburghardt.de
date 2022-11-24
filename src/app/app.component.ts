d873609acc320fb59b2a9515b06c908937518a7b
Andreas Burghardt <andreas-burghardt@gmx.net>
2022-11-24 10:28:17 +0100

    start header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   
-  images = ['laptop.jpg', 'sheets.jpg', 'music_pc.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
 
 }
